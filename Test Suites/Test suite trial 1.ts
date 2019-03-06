232e087fb652b8319c5dfbf609c88f4668c0c2a9
Jackal <jackal.wei@xteamstudio.com>
2019-03-06 14:33:20 +0800

    Add a test suite

---
+++ Test Suites/Test suite trial 1.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test suite trial 1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>8431eef1-1878-4122-a039-fa05ce9d9cf0</testSuiteGuid>
+   <testCaseLink>
+      <guid>d114a311-81ea-4ed0-9c42-aff52bbfdc6a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Trial1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
